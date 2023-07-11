46e00a73d65e05fd97b3a46fb9cbdbb0d10dfb7f
SagarPrasad160 <sagarprasad916@gmail.com>
2023-07-11 18:53:25 +0530

    add computing power algo

--- maths/index.js
+++ maths/index.js
@@ -24,4 +24,18 @@ function gcd(a, b) {
     min--;
   }
   return min;
+
+  // Computing power in O(logn)
+  function power(x, n) {
+    if (n === 0) {
+      return 1;
+    }
+    let result = power(x, Math.floor(n / 2));
+    result = result * result;
+    if (n % 2 === 0) {
+      return result;
+    } else {
+      return result * x;
+    }
+  }
 }
